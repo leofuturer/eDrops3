ef03c928801bb33c25a84963c2911832ee009bac
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2022-01-19 02:04:50 -0800

    Updated constants files for production store

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,16 +1,16 @@
 // product IDs
-export const controlSysId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ1OTU3Njc0NDM0OTA=';
-export const testBoardId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ1OTU3NjgwOTg4NTA=';
-export const univEwodChipId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ1OTk0ODUwMzg2MjY=';
+export const controlSysId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI2OTY4ODA=';
+export const testBoardId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI2MzEzNDQ=';
+export const univEwodChipId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI1OTg1NzY=';
 
 // product variant IDs
-export const univEwodChipWithCoverPlate = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMjIzMDM1NzQ5OTkzOA==';
-export const univEwodChipWithoutCoverPlate = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMjI0NDg1Njg4MTE4Ng==';
+export const univEwodChipWithCoverPlate = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTIzMzA5MjgwNDc4NA==';
+export const univEwodChipWithoutCoverPlate = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTIzMzA5MjgzNzU1Mg==';
 
 export const productIds = [controlSysId, testBoardId, univEwodChipId];
 
 // chip IDs
-export const ewodFabServiceId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ2MjI1Nzc4Mjc4NzQ=';
+export const ewodFabServiceId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI1NjU4MDg=';
 
 // chip variant IDs
-export const ewodFabServiceVariantId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMjMwMjU0ODA1ODE0Ng==';
+export const ewodFabServiceVariantId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTIzMzA5MjczOTI0OA==';
