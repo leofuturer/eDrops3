bb9220e07902498f0ed2cce624722c50484c915a
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2022-02-13 12:55:31 -0800

    Volume based pricing test

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,13 +1,19 @@
 // product IDs
 export const controlSysId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI2OTY4ODA=';
+export const controlSysId5 = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwNDA3NzM0ODg4MTY=';
+export const controlSysId10 = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwNDA3NzM4MTY0OTY='
 export const testBoardId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI2MzEzNDQ=';
+export const testBoardId5 = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwNDA3NzQ1MDQ2MjQ=';
+export const testBoardId10 = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwNDA3NzQ1NzAxNjA=';
 export const univEwodChipId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI1OTg1NzY=';
+export const univEwodChipId5 = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwNDA3NzU0NTQ4OTY=';
+export const univEwodChipId10 = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwNDA3NzU1ODU5Njg=';
 
 // product variant IDs
 export const univEwodChipWithCoverPlate = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTIzMzA5MjgwNDc4NA==';
 export const univEwodChipWithoutCoverPlate = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTIzMzA5MjgzNzU1Mg==';
 
-export const productIds = [controlSysId, testBoardId, univEwodChipId];
+export const productIds = [controlSysId, controlSysId5, controlSysId10, testBoardId, testBoardId5, testBoardId10, univEwodChipId, univEwodChipId5, univEwodChipId10];
 
 // chip IDs
 export const ewodFabServiceId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzcwMTE4ODI1NjU4MDg=';
