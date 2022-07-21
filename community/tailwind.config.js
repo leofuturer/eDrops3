f6a6692de39e8eaf4d071ba708ce6081bb88257f
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2022-07-21 00:10:32 -0400

    Fix linting issues

--- community/tailwind.config.js
+++ community/tailwind.config.js
@@ -1,12 +1,14 @@
+const lineClamp = require('@tailwindcss/line-clamp');
+
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     fontFamily: {
-      sans: ["Work Sans", "Inter", "sans-serif"],
+      sans: ['Work Sans', 'Inter', 'sans-serif'],
     },
     extend: {},
   },
-  plugins: [require('@tailwindcss/line-clamp')],
-}
+  plugins: [lineClamp],
+};
