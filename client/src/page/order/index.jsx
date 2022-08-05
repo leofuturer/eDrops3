50fbfabf5219ac5e23b034eef995f6147cbf237b
Shounak Kuiry <shounakkuiry@gmail.com>
2022-08-04 20:31:34 -0700

    Initial message model system

--- client/src/page/order/index.jsx
+++ client/src/page/order/index.jsx
@@ -69,6 +69,14 @@ class Orders extends React.Component {
     const WindowForOrderDetail = window.open(redirectUrl, '_blank', strWindowFeatures);
   }
 
+  handleChat(e){
+    const originalOrderId = e.target.id;
+    const orderId = Number(originalOrderId.replace(/[^0-9]/ig, ''));
+    const redirectUrl = `/subpage/order-chat?id=${orderId}`;
+    const strWindowFeatures = 'width=1200px, height=900px';
+    const WindowForOrderDetail = window.open(redirectUrl, '_blank', strWindowFeatures);
+  }
+
   render() {
     return (
       <div>
@@ -101,6 +109,7 @@ class Orders extends React.Component {
                     <th>Process Status</th>
                     <th>Total Price</th>
                     <th className="icon-center">Other Details</th>
+                    <th className="icon-center">Chat</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -116,6 +125,9 @@ class Orders extends React.Component {
                         </td>
                         <td className="icon-center">
                           <i className="fa fa-commenting" id={`worker-order${item.id}`} onClick={this.handleDetail} />
+                        </td>
+                        <td className="icon-center">
+                          <i className="fa fa-commenting" id={`worker-order${item.id}`} onClick={this.handleChat} />
                         </td>
                       </tr>
                     ))
