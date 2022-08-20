6e13031df8d8795123e404535f1b05bddbace901
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2022-08-20 15:41:28 -0700

    Derandomize seed data

--- server/src/interceptors/index.ts
+++ server/src/interceptors/index.ts
@@ -3,4 +3,3 @@ export * from './order-item-create.interceptor';
 export * from './order-chip-update.interceptor';
 export * from './author.interceptor';
 export * from './post-like.interceptor';
-export * from './order-chip-update.interceptor';
