ad82fef70637a8cb98cfc631693560fe9640f3b0
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-01-14 02:00:48 -0800

    Clean up files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { CookiesProvider } from 'react-cookie';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-import PusherContextProvider from './context/PusherContext';
+// import PusherContextProvider from './context/PusherContext';
 import ShopifyContextProvider from './context/ShopifyContext';
 import './global.css';
 import RouteMap from './router/routeMap';
@@ -13,11 +13,11 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <CookiesProvider>
       <HelmetProvider>
         <ShopifyContextProvider>
-          <PusherContextProvider>
+          {/* <PusherContextProvider> */}
             <BrowserRouter>
               <RouteMap />
             </BrowserRouter>
-          </PusherContextProvider>
+          {/* </PusherContextProvider> */}
         </ShopifyContextProvider>
       </HelmetProvider>
     </CookiesProvider>
