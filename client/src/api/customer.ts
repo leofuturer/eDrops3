f77ee6278043468011aca090625474e51fb48f8b
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-05-19 04:18:09 -0700

    Fixes #112 and #113

--- client/src/api/customer.ts
+++ client/src/api/customer.ts
@@ -1,6 +1,18 @@
+import { Order, OrderInfo } from "@/types";
+import request from "./lib/api";
+import { customer } from "./lib/newServerConfig";
+
 // Get customer info
 export function getCustomerInfo(customerId: string) {
 
 }
 
+
+export function getCutomerOrders(customerId: string, completed: boolean = true): Promise<OrderInfo[]> {
+  return request(customer.getOrdersById(customerId), 'GET', { where: { orderComplete: completed } }, true)
+    .then((res) => {
+      // console.log(res.data)
+      return res.data as OrderInfo[];
+    })
+}
 // Get customer 
