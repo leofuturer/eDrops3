222f3a3868e6dc55d63252bc515f29cd2bea562c
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-07-09 20:14:59 -0400

    Fixes a couple of cart reactivity issues

--- client/src/context/CartContext.tsx
+++ client/src/context/CartContext.tsx
@@ -8,12 +8,13 @@ import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 import { CheckoutLineItemInput, Product } from 'shopify-buy';
 
+
 const useCart = () => {
   const [cart, setCart] = useState<DTO<OrderInfo>>({} as DTO<OrderInfo>);
   const [fetchingCart, setFetchingCart] = useState(false);
   const [productOrders, setProductOrders] = useState<DTO<OrderProduct>[]>([]);
   const [chipOrders, setChipOrders] = useState<DTO<OrderChip>[]>([]);
-  // const [numItems, setNumItems] = useState(0);
+  const [numItems, setNumItems] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
   const enabled = true;
 
@@ -40,7 +41,7 @@ const useCart = () => {
 
   // create cart if it doesn't exist, otherwise get cart info
   useEffect(() => {
-    if(cookies.userId && !fetchingCart) {
+    if (cookies.userId && !fetchingCart) {
       fetchCart();
     }
   }, [cookies.userId]);
@@ -53,19 +54,19 @@ const useCart = () => {
   }, [cart]);
 
   // count number of items in cart
-  // useEffect(() => {
-  //   if (cart && 'orderProducts' in cart && 'orderChips' in cart) {
-  //     const numProducts = cart.orderProducts?.length ? cart.orderProducts.reduce((acc, item) => acc + item.quantity, 0) : 0;
-  //     const numChips = cart.orderChips ? cart.orderChips.reduce((acc, item) => acc + item.quantity, 0) : 0;
-  //     setNumItems(numProducts + numChips);
-  //   }
-  // }, [cart]);
+  useEffect(() => {
+    if (cart) {
+      const numProducts = ('orderProducts' in cart) ? cart.orderProducts.reduce((acc, item) => acc + item.quantity, 0) : 0;
+      const numChips = ('orderChips' in cart) ? cart.orderChips.reduce((acc, item) => acc + item.quantity, 0) : 0;
+      setNumItems(numProducts + numChips);
+    }
+  }, [cart]);
 
   // calculate total price of cart
   useEffect(() => {
-    if (cart && 'orderProducts' in cart && 'orderChips' in cart) {
-      const productPrice = cart.orderProducts?.length ? cart.orderProducts.reduce((acc, item) => acc + item.quantity * item.price, 0) : 0;
-      const chipPrice = cart.orderChips ? cart.orderChips.reduce((acc, item) => acc + item.quantity * item.price, 0) : 0;
+    if (cart) {
+      const productPrice = ('orderProducts' in cart) ? cart.orderProducts.reduce((acc, item) => acc + item.quantity * item.price, 0) : 0;
+      const chipPrice = ('orderChips' in cart) ? cart.orderChips.reduce((acc, item) => acc + item.quantity * item.price, 0) : 0;
       setTotalPrice(productPrice + chipPrice);
     }
   }, [cart]);
@@ -73,7 +74,6 @@ const useCart = () => {
   useEffect(() => {
     console.log(cart);
   }, [cart]);
-
 
   // add to shopify cart, and then add to our own cart
   async function addProduct(product: Product, quantity: number): Promise<void> {
@@ -87,13 +87,17 @@ const useCart = () => {
       variantId: product.variants[0].id,
     }
 
-    return api.order.addProductOrder(cart.id as number, data)
-      .then((product) => api.order.getProductOrders(cart.id as number))
-      .then((products) => {
-        // console.log(res);
-        setProductOrders(products);
-        navigate(ROUTES.ManageCart);
-      }).catch((err) => console.error(err));
+    // return api.order.addProductOrder(cart.id as number, data)
+    //   .then((product) => api.order.getProductOrders(cart.id as number))
+    //   .then((products) => {
+    //     // console.log(res);
+    //     setProductOrders(products);
+    //     navigate(ROUTES.ManageCart);
+    //   }).catch((err) => console.error(err));
+    return api.order.addProductOrder(cart.id as number, data).then(() => {
+      fetchCart();
+      navigate(ROUTES.ManageCart);
+    }).catch((err) => console.error(err));
   }
 
   async function addChip(chip: Product, quantity: number, customAttrs: { material: Material, wcpa: string, fileInfo: DTO<FileInfo> }): Promise<void> {
@@ -115,48 +119,64 @@ const useCart = () => {
         value: customAttrs.fileInfo.fileName,
       }]
     }
-    return api.order.addChipOrder(cart.id as number, data)
-      .then((res) => api.order.getChipOrders(cart.id as number))
-      .then((chips) => {
-        setChipOrders(chips);
-        navigate(ROUTES.ManageCart);
-      }).catch((err) => console.error(err));
+    // return api.order.addChipOrder(cart.id as number, data)
+    //   .then((res) => api.order.getChipOrders(cart.id as number))
+    //   .then((chips) => {
+    //     setChipOrders(chips);
+    //     navigate(ROUTES.ManageCart);
+    //   }).catch((err) => console.error(err));
+    return api.order.addChipOrder(cart.id as number, data).then(() => {
+      fetchCart();
+      navigate(ROUTES.ManageCart);
+    }).catch((err) => console.error(err));
   }
 
   function editProductQuantity(product: DTO<OrderProduct>) {
-    return api.order.updateProductOrder(cart.id as number, product.id, product)
-      .then((res) => api.order.getProductOrders(cart.id as number))
-      .then((products) => {
-        // console.log(res);
-        setProductOrders(products);
-      })
+    // return api.order.updateProductOrder(cart.id as number, product.id, product)
+    //   .then((res) => api.order.getProductOrders(cart.id as number))
+    //   .then((products) => {
+    //     // console.log(res);
+    //     setProductOrders(products);
+    //   })
+    return api.order.updateProductOrder(cart.id as number, product.id, product).then(() => {
+      fetchCart();
+    }).catch((err) => console.error(err));
   }
 
   function editChipQuantity(chip: DTO<OrderChip>) {
-    return api.order.updateChipOrder(cart.id as number, chip.id, chip)
-      .then((res) => api.order.getChipOrders(cart.id as number))
-      .then((chips) => {
-        // console.log(res);
-        setChipOrders(chips);
-      })
+    // return api.order.updateChipOrder(cart.id as number, chip.id, chip)
+    //   .then((res) => api.order.getChipOrders(cart.id as number))
+    //   .then((chips) => {
+    //     // console.log(res);
+    //     setChipOrders(chips);
+    //   })
+    return api.order.updateChipOrder(cart.id as number, chip.id, chip).then(() => {
+      fetchCart();
+    }).catch((err) => console.error(err));
   }
 
   function removeProduct(product: DTO<OrderProduct>) {
-    return api.order.deleteProductOrder(cart.id as number, product.id)
-      .then((res) => api.order.getProductOrders(cart.id as number))
-      .then((products) => {
-        // console.log(res);
-        setProductOrders(products);
-      })
+    // return api.order.deleteProductOrder(cart.id as number, product.id)
+    //   .then((res) => api.order.getProductOrders(cart.id as number))
+    //   .then((products) => {
+    //     // console.log(res);
+    //     setProductOrders(products);
+    //   })
+    return api.order.deleteProductOrder(cart.id as number, product.id).then(() => {
+      fetchCart();
+    }).catch((err) => console.error(err));
   }
 
   function removeChip(chip: DTO<OrderChip>) {
-    return api.order.deleteChipOrder(cart.id as number, chip.id)
-      .then((res) => api.order.getChipOrders(cart.id as number))
-      .then((chips) => {
-        // console.log(res);
-        setChipOrders(chips);
-      })
+    // return api.order.deleteChipOrder(cart.id as number, chip.id)
+    //   .then((res) => api.order.getChipOrders(cart.id as number))
+    //   .then((chips) => {
+    //     // console.log(res);
+    //     setChipOrders(chips);
+    //   })
+    return api.order.deleteChipOrder(cart.id as number, chip.id).then(() => {
+      fetchCart();
+    }).catch((err) => console.error(err));
   }
 
   function checkout(address: DTO<Address>): Promise<any> {
@@ -175,11 +195,12 @@ const useCart = () => {
 
   return {
     enabled,
-    // numItems,
+    numItems,
     totalPrice,
     cart,
     productOrders,
     chipOrders,
+    fetchCart,
     addProduct,
     addChip,
     editProductQuantity,
