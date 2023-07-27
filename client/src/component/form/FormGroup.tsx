d1042993b334fe5640a8b83ea36ed9a7e682de69
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-07-26 22:26:11 -0400

    Fix signup bugs

--- client/src/component/form/FormGroup.tsx
+++ client/src/component/form/FormGroup.tsx
@@ -32,7 +32,7 @@ function FormGroup({ name, displayName, required = false, type = "text", autoCom
                     setShowConfirmPassword(false);
                   }} 
                   disabled={disabled}/>
-                {showConfirmPassword ? <EyeIcon className="w-4 absolute right-1 text-gray-600 cursor-pointer" onClick={() => setShowConfirmPassword(!showConfirmPassword)} /> : <EyeSlashIcon className="w-4 absolute right-1 text-gray-600 cursor-pointer" onClick={() => setShowConfirmPassword(!showConfirmPassword)} />}
+                {showConfirmPassword ? <EyeSlashIcon className="w-4 absolute right-1 text-gray-600 cursor-pointer" onClick={() => setShowConfirmPassword(!showConfirmPassword)} /> : <EyeIcon className="w-4 absolute right-1 text-gray-600 cursor-pointer" onClick={() => setShowConfirmPassword(!showConfirmPassword)} />}
               </div>
             ) : (
             <input
