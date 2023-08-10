d8db673ea3850d88945196357958c95c0dacbecc
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-08-09 23:08:53 -0400

    Improve form inputs

--- community/src/pages/signup/index.tsx
+++ community/src/pages/signup/index.tsx
@@ -40,8 +40,8 @@ export function Signup() {
 	};
 
 	return (
-		<div className="w-full h-full flex flex-col justify-center items-center">
-			<div className="flex flex-col items-center border-2 shadow-xl rounded-md pt-4 pb-10 w-2/3">
+		<div className="w-full h-full flex flex-col justify-center items-center px-8">
+			<div className="flex flex-col items-center sm:border-2 sm:shadow-xl rounded-md pt-4 pb-10 px-4 w-full sm:w-2/3">
 				<div className="max-w-xs">
 					<img src="/static/img/edroplets-banner.png" alt="eDroplets Logo" className="" />
 				</div>
@@ -62,7 +62,7 @@ export function Signup() {
 						}
 					)}
 				>
-					<Form className="flex flex-col items-center justify-center space-y-4 w-2/3" >
+					<Form className="flex flex-col items-center justify-center space-y-4 w-full md:w-2/3" >
 						<FormInput name="email" displayName="Email" autoComplete="email" />
 						<FormInput name="username" displayName="Username" autoComplete="username" />
 						<FormInput name="password" displayName="Password" type="password" autoComplete="new-password" />
@@ -70,89 +70,6 @@ export function Signup() {
 						<button type="submit" className="w-full h-16 bg-primary text-white text-lg rounded-md mt-8">Sign Up</button>
 					</Form>
 				</Formik>
-				{/* <form
-					className="flex flex-col items-center justify-center space-y-4"
-					onSubmit={(e) => handleLogin(e)}
-				>
-					<div className="w-full">
-						<label htmlFor="email" className="text-sm">
-							Email
-						</label>
-						<input
-							id="email"
-							type="text"
-							className="w-full h-8 p-2 border-2 rounded-md"
-							value={formData.email}
-							onChange={(e) =>
-								setFormData({
-									...formData,
-									email: e.target.value,
-								})
-							}
-						/>
-					</div>
-					<div className="w-full">
-						<label htmlFor="username" className="text-sm">
-							Username
-						</label>
-						<input
-							id="username"
-							type="text"
-							className="w-full h-8 p-2 border-2 rounded-md"
-							value={formData.username}
-							onChange={(e) =>
-								setFormData({
-									...formData,
-									username: e.target.value,
-								})
-							}
-						/>
-					</div>
-					<div className="w-full">
-						<label htmlFor="password" className="text-sm">
-							Password
-						</label>
-						<input
-							id="password"
-							type="password"
-							className="w-full h-8 p-2 border-2 rounded-md"
-							value={formData.password}
-							onChange={(e) =>
-								setFormData({
-									...formData,
-									password: e.target.value,
-								})
-							}
-						/>
-					</div>
-					<div className="w-full">
-						<label htmlFor="confirmPassword" className="text-sm">
-							Confirm Password
-						</label>
-						<input
-							id="confirmPassword"
-							type="password"
-							className="w-full h-8 p-2 border-2 rounded-md"
-							value={formData.confirmPassword}
-							onChange={(e) =>
-								setFormData({
-									...formData,
-									confirmPassword: e.target.value,
-								})
-							}
-						/>
-					</div>
-					<div className="w-full flex flex-col justify-center items-center space-y-2">
-						<button className="w-full h-8 border-2 rounded-md mt-8">
-							Signup
-						</button>
-						<ul className="list-none text-red-700 text-xs text-center">
-							{errors?.map((err, index) => (
-								<li key={index}>{err}</li>
-							))}
-						</ul>
-					</div>
-				</form> */}
 			</div>
 		</div>
 	);
