4c08ce894b52b66d90c72d07c12f02fedb3f5f58
Kenny477 <53405834+Kenny477@users.noreply.github.com>
2023-10-01 15:11:18 -0700

    Update seed data

--- community/src/components/project/ProjectPreview.tsx
+++ community/src/components/project/ProjectPreview.tsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Project as ProjectType } from "@edroplets/api";
+import { Project as ProjectType, api } from "@edroplets/api";
 
 function ProjectPreview({ project }: { project: ProjectType }) {
+	const [coverImageId, setCoverImageId] = useState(-1);
+
+	useEffect(() => {
+		api.project.getProjectFiles(project.id).then((files) => {
+			const images = files.filter((file) => file.fileType === "image");
+			if (images.length === 0) {
+				setCoverImageId(-1);
+			}
+			else {
+				setCoverImageId(images[0].id);
+			}
+		})
+	}, [project.id]);
+
 	return (
 		<NavLink
 			to={`/project/${project.id}`}
 			className="w-full flex flex-col items-center cursor-pointer rounded-2xl mb-4 bg-white shadow-2xl"
 		>
-			<div className="w-full h-40 rounded-t-2xl bg-gray-400">
-				{/* <img src={project.image} alt={project.title} className="w-full h-full rounded-2xl" /> */}
-			</div>
+			{coverImageId !== -1 &&
+				<div className="w-full h-40 rounded-t-2xl bg-gray-400">
+					<img src={`/api/project-files/${coverImageId}/download`} alt={project.title} className="w-full h-full rounded-2xl" />
+				</div>
+			}
 			<div className="w-full flex flex-col p-4">
 				<div className="flex flex-row justify-between">
 					<h3>{project.title}</h3>
 					<h3>{project.author}</h3>
 				</div>
-
-				{/* <p>{project.content}</p> */}
+				{coverImageId == -1 && <p className="line-clamp-2">{project.content}</p> }
 				<p>{new Date(project.datetime).toLocaleDateString()}</p>
 			</div>
 		</NavLink>
