0c4d8386d1b3ddab6c04d9a12e3664964c8bbea3
cdevadhar <cdevadhar@gmail.com>
2024-02-22 22:39:25 -0800

    added edit/delete dropdown to post previews
    TODO: factor this stuff out into a reusable component

--- community/src/components/forum/PostPreview.tsx
+++ community/src/components/forum/PostPreview.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Post, Project, api } from "@edroplets/api";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
-import { BookmarkIcon, ChatBubbleBottomCenterTextIcon, ChatBubbleLeftIcon, HandThumbUpIcon } from "@heroicons/react/24/outline";
+import { BookmarkIcon, ChatBubbleBottomCenterTextIcon, ChatBubbleLeftIcon, HandThumbUpIcon, EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 import { useCookies } from "react-cookie";
 import { AxiosError } from "axios";
 
@@ -10,6 +10,8 @@ function PostPreview({ post }: { post: Post }) {
 
 	const [saved, setSaved] = useState<boolean>(false);
 	const [cookies] = useCookies(["userId"]);
+	const [dropdownVisible, setDropdownVisible] = useState<boolean>(false);
+	const [deleteModalVisible, setDeleteModalVisible] = useState<boolean>(false);
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -36,16 +38,59 @@ function PostPreview({ post }: { post: Post }) {
 	return (
 		<NavLink
 			to={`/forum/${post.id}`}
-			className="w-full flex flex-col items-center cursor-pointer rounded-2xl mb-4 bg-white shadow-2xl"
+			className="w-full flex flex-col items-center cursor-pointer rounded-2xl mb-4 bg-white shadow-2xl z-0"
 		>
 			<div className="w-full flex flex-col space-y-4 p-4">
 				<div className="flex flex-row justify-between items-center">
 					<h3 className="text-xl">{post.title}</h3>
-					<BookmarkIcon
-						className={`w-8 h-8 cursor-default ${saved ? "fill-black" : ""
-							}`}
-						onClick={(e) => handleSave(e)}
-					/>
+					<div className="flex flex-row">
+						{
+							(post.userId===cookies.userId) ?
+								<div className="flex flex-col">
+									<EllipsisHorizontalIcon
+										className={`w-10 h-10 z-100 cursor-default`}
+										onClick={(e) => {
+											e.preventDefault();
+											setDropdownVisible(!dropdownVisible);
+										}}
+									/>
+									<div style={{display: dropdownVisible ? "block" : "none"}} className="absolute mt-8 -ml-9">
+										<ul className="text-white bg-slate-700 p-2 px-3 rounded">
+											<li>
+												<button
+													onClick={(e)=> {
+														e.preventDefault();
+														console.log("editing");
+														navigate(`/forum/new?edit=true&id=${post.id}`);
+													}}
+												>
+													Edit
+												</button>
+											</li>
+											<li>
+												<button
+													onClick={(e) => {
+														e.preventDefault();
+														setDeleteModalVisible(true);
+														setDropdownVisible(false);
+													}}
+												>
+													Delete
+												</button>
+											</li>
+										</ul>
+									</div>
+								</div> 
+								: 
+								<></>
+						}
+						<BookmarkIcon
+							className={`w-8 h-8 cursor-default ${saved ? "fill-black" : ""
+								}`}
+							onClick={(e) => handleSave(e)}
+						/>
+					</div>
+					
 				</div>
 				<div className="flex flex-row space-x-2 items-center">
 					<div className="flex items-center space-x-1"><UserCircleIcon className="h-6 text-gray-500" /><h3>{post.author}</h3></div>
