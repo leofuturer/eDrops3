7994de6f1ca753a6ce2815b6fbd871abafa611c0
aditya-am-murthy <aditya.am.murthy@gmail.com>
2024-05-30 13:55:52 -0700

    adding additional login features

--- community/src/components/NavTop.tsx
+++ community/src/components/NavTop.tsx
@@ -46,8 +46,8 @@ export function NavTop() {
 
 	return (
 		<div className="">
-			<header className="flex flex-row items-center bg-sky-800 h-[80px] w-full text-white px-32 justify-between">
-				<div id="navLeft">
+			<header className="flex flex-row items-center h-[80px] w-full text-white px-32 justify-between bg-primary">
+				<div id="navLeft" className="flex flex-row items-center py-4 h-full space-x-8 text-xl lg:text-2xl font-medium">
 					<NavLink
 						to="/"
 						className="flex flex-row items-center justify-center"
@@ -59,35 +59,32 @@ export function NavTop() {
 							className="max-h-[50px]"
 						/>
 					</NavLink>
-				</div>
-				<div
-					id="navRight"
-					className="flex flex-row items-center justify-center space-x-8"
-				>
 					<NavLink to="/projects">
 						<div className="flex flex-col items-center">
-							{path === "/projects" ? <FolderOpenIcon className="h-6 w-6" /> : <FolderOpenIconOutline className="h-6 w-6" />}
+							{path === "/projects"}
 							<h1>Projects</h1>
 						</div>
 					</NavLink>
 					<NavLink to="/forum">
 						<div className="flex flex-col items-center">
-							{path === "/forum" ? <ChatBubbleLeftRightIcon className="h-6 w-6" /> : <ChatBubbleLeftRightIconOutline className="h-6 w-6" />}
+							{path === "/forum"}
 							<h1>Forum</h1>
 						</div>
 					</NavLink>
+				</div>
+				<div id="navRight"className="flex flex-row items-center justify-center space-x-8  text-xl lg:text-2xl font-medium">
 					{notLoggedIn ? (
 						<>
-							<NavLink to="/signup">
-								<div className="flex flex-col items-center">
-									{path === "/signup" ?<UserPlusIcon className="h-6 w-6" /> : <UserPlusIconOutline className="h-6 w-6" />}
-									<h1>Sign up</h1>
+							<NavLink to="/login">
+								<div className="flex items-center">
+                  					{path === "/login"}
+									<h1>Login</h1>
 								</div>
 							</NavLink>
-							<NavLink to="/login">
+							<NavLink to="/signup">
 								<div className="flex flex-col items-center">
-                  {path === "/login" ? <ArrowLeftCircleIcon className="h-6 w-6" /> : <ArrowLeftOnRectangleIconOutline className="h-6 w-6" />}
-									<h1>Sign in</h1>
+									{path === "/signup"}
+									<h1>Sign up</h1>
 								</div>
 							</NavLink>
 						</>
