77546c8381eaff16761b137438fb50bb02f31cea
aditya-am-murthy <aditya.am.murthy@gmail.com>
2024-05-30 13:55:52 -0700

    adding additional login features

--- client/src/page/signup/index.tsx
+++ client/src/page/signup/index.tsx
@@ -44,32 +44,37 @@ export function Register() {
   }
 
   return (
-    <div className="flex items-center justify-center py-10">
+    <div className="w-full h-full flex flex-col justify-center items-center">
       <SEO
         title="eDroplets | Register"
         description=""
         metadata={metadata}
       />
-      <div className="flex flex-col shadow-box-sm rounded-lg py-4 w-1/2 px-20 space-y-2">
-        <h3 className="text-secondary text-2xl text-center font-bold border-b-2 pb-2 border-secondary">Sign Up</h3>
+      <div className="flex flex-col items-center outline outline-1 outline-gray-300 shadow-xl rounded-md pt-4 pb-10 w-1/2 bg-white">
+        
+        <h3 className="text-primary text-2xl text-center font-medium border-b-2 pb-2 border-primary">Sign Up</h3>
         <Formik
           validationSchema={CustomerSchema}
           initialValues={initialInfo}
-          onSubmit={(values, actions) => CustomerSubmitSchema.validate(values, { abortEarly: false }).then(() => {
-            handleRegister({ ...values } as DTO<Customer & User & Address>)
-          }).catch((err: ValidationError) => {
-            const errors = err.inner.reduce((acc: object, curr: ValidationError) => {
-              return {
-                ...acc,
-                [curr.path as string]: curr.message,
-              };
-            }, {});
-            actions.setErrors(errors);
-          })}
+          onSubmit={(values, actions) =>
+            CustomerSubmitSchema.validate(values, { abortEarly: false })
+              .then(() => {
+                handleRegister({ ...values } as DTO<Customer & User & Address>)
+              })
+              .catch((err: ValidationError) => {
+                const errors = err.inner.reduce((acc: object, curr: ValidationError) => {
+                  return {
+                    ...acc,
+                    [curr.path as string]: curr.message,
+                  };
+                }, {});
+                actions.setErrors(errors);
+              })
+          }
         >
           <Form className="flex flex-col space-y-2">
             <p className="text-xs text-gray-500">Fields with * are required</p>
-            <FormGroup name="email" required type="email" autoComplete="email" />
+            <FormGroup name="email" required type="email" autoComplete="email"/>
             <FormGroup name="username" required autoComplete="username" />
             <FormGroup name="password" required type="password" autoComplete="new-password" />
             <FormGroup name="confirmPassword" required type="password" autoComplete="new-password" />
@@ -77,7 +82,7 @@ export function Register() {
             <FormGroup name="lastName" required autoComplete="family-name" />
             <div className="grid grid-cols-4 gap-4 items-center">
               <label htmlFor="customerType" className="text-sm font-bold">User Type*</label>
-              <Field id="customerType" name="customerType" as="select" className="outline outline-1 shadow-inner focus:shadow-box-sm  rounded px-1 py-1 col-span-2 outline-gray-400 focus:shadow-primary_light focus:outline-primary_light">
+              <Field id="customerType" name="customerType" as="select" className="outline outline-1 shadow-inner focus:shadow-box-sm rounded px-1 py-1 col-span-2 outline-gray-400 focus:shadow-primary_light focus:outline-primary_light">
                 <option value="person">Person</option>
                 <option value="business">Company</option>
               </Field>
@@ -90,7 +95,7 @@ export function Register() {
             <FormGroup name="state" displayName="State or Province" autoComplete="address-level1" />
             <FormGroup name="zipCode" displayName="Zip or Postal Code" autoComplete="postal-code" />
             <FormGroup name="country" autoComplete="country" />
-            <button type="submit" className="bg-secondary text-white rounded-lg px-4 py-2">Submit</button>
+            <button type="submit" className="w-full h-16 bg-primary text-white text-lg rounded-md mt-8">Submit</button>
           </Form>
         </Formik>
         <div className="form-group">
@@ -99,6 +104,6 @@ export function Register() {
           </small>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
