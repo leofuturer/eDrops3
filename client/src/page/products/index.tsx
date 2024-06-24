af8aaf18327a8d4b5f18f96354e185c036c576eb
aditya-am-murthy <aditya.am.murthy@gmail.com>
2024-06-24 13:40:23 -0700

    Added product page's loading buffer fixes

--- client/src/page/products/index.tsx
+++ client/src/page/products/index.tsx
@@ -1,14 +1,14 @@
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Await } from 'react-router-dom';
 import { Product } from 'shopify-buy';
 import SEO from '@/component/header/seo';
 import ItemCard from './ItemCard';
 import ItemLoad from './ItemLoad';
 import { metadata } from './metadata';
-import { api } from '@edroplets/api';
+import { useProducts } from './ProductsContext';
 
 export function Products() {
-  const products = api.product.getAll();
+  const { products, loading, error } = useProducts();
 
   return (
     <div className="flex flex-col items-center space-y-10 mb-10">
@@ -19,16 +19,13 @@ export function Products() {
       />
       <h3 className="border-b-2 border-secondary text-secondary w-1/3 min-w-min text-4xl font-bold text-center py-8">Products</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 w-full px-8 lg:px-0 lg:w-3/4">
-        <Suspense fallback={<><ItemLoad /><ItemLoad /><ItemLoad /></>}>
-          <Await
-            resolve={products}
-            errorElement={<><ItemLoad /><ItemLoad /><ItemLoad /></>}
-            children={(resolvedProducts) => resolvedProducts.map((p: Product) =>
-              <ItemCard product={p} key={p.id} />
-            )}
-          />
-        </Suspense>
+        {loading && <ItemLoad />}
+        {products && products.map((product: Product) => (
+            <ItemCard product={product} key={product.id} />
+          ))}
       </div>
     </div>
   );
 }
+
+export default Products;
