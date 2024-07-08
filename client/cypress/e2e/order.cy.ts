16177f24e87d63753fd6e1882beff7042502cc64
cdevadhar <cdevadhar@gmail.com>
2024-07-08 15:53:54 -0700

    place order cypress test

--- client/cypress/e2e/order.cy.ts
+++ client/cypress/e2e/order.cy.ts
@@ -2,6 +2,17 @@
 describe("Placing order tests", () => {
     it('Place an order', () => {
         cy.login("customerA", "edropTest123");
-        
+        cy.url().should('include', '/home');
+        cy.get('a[href*="/upload"]').filter(':visible').first().click();
+        cy.url().should('include', '/upload');
+        cy.get('input[type=file]').selectFile('./cypress/test.dxf', {force: true});
+        cy.get('button').contains('Upload File').click();
+        cy.get('button').contains('Upload this file').click();
+        cy.get('button').contains('Proceed to fabrication').click();
+
+        cy.url().should('include', '/chip-fab');
+        cy.get('button').contains('Add to Cart').click();
+        cy.url().should('include', 'manage/cart');
+        cy.get('button').contains('Checkout').click();
     })
 })
